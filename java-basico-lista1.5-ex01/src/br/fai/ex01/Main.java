3acae1a9eebd52706e69b47f0fb4d80f06f25840
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-02-26 15:15:45 -0300

    feat: adicionado leitura de valores

--- java-basico-lista1.5-ex01/src/br/fai/ex01/Main.java
+++ java-basico-lista1.5-ex01/src/br/fai/ex01/Main.java
@@ -1,11 +1,26 @@
 package br.fai.ex01;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		Main app = new Main();
 		app.start();
 	}
 	private void start() {
+		int valores[] = lerValores();
+
+	}
+	
+	private int[] lerValores() {
+		Scanner scanner = new Scanner(System.in);
+		int valores[] = new int[4];
 		
+		for (int i = 0; i < 4; i++) {
+			System.out.printf("Digite um valor inteiro %d: ", i + 1);
+			valores[i] = scanner.nextInt();
+		}
+		
+		return valores;
 	}
 }
