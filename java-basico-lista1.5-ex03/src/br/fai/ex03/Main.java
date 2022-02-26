aa194577c01882dec547e29c7f4a5d99d4f87b1e
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-02-26 16:29:58 -0300

    feat: adicionado leitura de palavras

--- java-basico-lista1.5-ex03/src/br/fai/ex03/Main.java
+++ java-basico-lista1.5-ex03/src/br/fai/ex03/Main.java
@@ -1,5 +1,7 @@
 package br.fai.ex03;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		Main app = new Main();
@@ -7,6 +9,16 @@ public class Main {
 	}
 	
 	private void start() {
+		String palavra[] = lerPalavra();
+	}
+	
+	private String[] lerPalavra() {
+		String palavra;
+		Scanner scanner = new Scanner(System.in);
 		
+		System.out.print("Digite uma palavra ou frase: ");
+		palavra = scanner.nextLine();
+		
+		return palavra.split("");
 	}
 }
