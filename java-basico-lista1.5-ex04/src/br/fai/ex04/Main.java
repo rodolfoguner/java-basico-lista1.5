cd8cb4be1ae78f53b9f38fd0736cd36ccc0a92b9
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-02-26 22:58:59 -0300

    feat: adicionado leitura de valor e conversao pra inteiro

--- java-basico-lista1.5-ex04/src/br/fai/ex04/Main.java
+++ java-basico-lista1.5-ex04/src/br/fai/ex04/Main.java
@@ -1,12 +1,25 @@
 package br.fai.ex04;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Main app = new Main();
+		app.start();
 	}
 	
 	private void start() {
+		String palavraDigitada;
+		int numeroConvertido;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("String: ");
+		palavraDigitada = scanner.nextLine();
+		
+		numeroConvertido = Integer.parseInt(palavraDigitada);
+		
+		System.out.printf("Int: %d", numeroConvertido);
 		
 	}	
 }
