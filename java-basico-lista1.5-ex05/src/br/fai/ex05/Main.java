3648e2d8beeff52950924ab22835a8430f29c242
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-02-27 00:29:25 -0300

    feat: adicionado leitura de palavra e exibicao da penultima

--- java-basico-lista1.5-ex05/src/br/fai/ex05/Main.java
+++ java-basico-lista1.5-ex05/src/br/fai/ex05/Main.java
@@ -1,5 +1,7 @@
 package br.fai.ex05;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		Main app = new Main();
@@ -7,7 +9,28 @@ public class Main {
 	}
 	
 	private void start() {
+		String palavra;
+		String frase[];
+		int tamFrase;
+		Scanner scannner = new Scanner(System.in);
 		
+		System.out.print("Digite uma palavra ou uma frase: ");
+		palavra = scannner.nextLine();
+		
+		frase = palavra.trim().split(" ");
+		tamFrase = frase.length;
+		
+		if (tamFrase == 1 && frase[0].equals("")) {
+			System.out.println("Nada foi digitado.");
+		} else if (tamFrase == 1){
+			System.out.println("Penultima palavra: " + frase[tamFrase - 1]);
+		} else {
+			System.out.println("Penultima palavra: " + frase[tamFrase - 2]);
+		}
+		
+		if (tamFrase > 4) {
+			System.out.println("Texto muito longo.");
+		}
 	}
 	
 }
