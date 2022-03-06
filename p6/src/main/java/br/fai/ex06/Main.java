96e3439556f6e6a32834c8aa6f5887e05464b45d
Rodolfo Mamud <rodolfo.guner@outlook.com>
2022-03-06 17:25:05 -0300

    feat: adicionado calculo de media e concatenacao de array

--- p6/src/main/java/br/fai/ex06/Main.java
+++ p6/src/main/java/br/fai/ex06/Main.java
@@ -1,4 +1,5 @@
 package br.fai.ex06;
+import org.apache.commons.lang3.ArrayUtils;
 
 public class Main {
 	public static void main(String[] args) {
@@ -7,6 +8,24 @@ public class Main {
 	}
 	
 	private void start() {
+		int array1[] = {50, -20, 0, 40};
+		int array2[] = {5, -50, 10, 60};
+		int arrayCombinado[];
+		float soma = 0;
+		float media = 0;
 		
+		arrayCombinado = ArrayUtils.addAll(array1, array2);
+		
+		for (int num: arrayCombinado) {
+			soma += num;
+		}
+		
+		media = soma / arrayCombinado.length;
+		
+		System.out.println("Valores do array: " + ArrayUtils.toString(array1));
+		System.out.println("Valores do array: " + ArrayUtils.toString(array2));
+		System.out.println("Arrays combinados: " + ArrayUtils.toString(arrayCombinado));
+		System.out.printf("Soma: %.0f", soma);
+		System.out.printf("\nMedia: %.2f", media);
 	}
 }
