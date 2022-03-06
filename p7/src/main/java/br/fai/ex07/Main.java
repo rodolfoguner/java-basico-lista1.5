db99ab7ad8c40a6694bea8167527797c7c4770aa
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-03-06 18:53:00 -0300

    feat: adicionado metodo para ler valores

--- p7/src/main/java/br/fai/ex07/Main.java
+++ p7/src/main/java/br/fai/ex07/Main.java
@@ -1,5 +1,11 @@
 package br.fai.ex07;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.commons.lang3.ArrayUtils;
+
 public class Main {
 	public static void main(String[] args) {
 		Main app = new Main();
@@ -7,6 +13,41 @@ public class Main {
 	}
 	
 	private void start() {
+		int[] array1 = lerValoresArray(1);
+		int[] array2 = lerValoresArray(2);
+		int arrayCombinado[];
 		
+		
+	}
+	
+	private int[] lerValoresArray(int numArray) {
+		String opcao = "";
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		Integer arrayObtido[];
+		
+		while (true) {
+			int num = 0;
+			
+			System.out.print("Digite um número de -100 até 100 para lista " + numArray + " ou sair: ");
+			opcao = scanner.nextLine().trim();
+
+			if (opcao.equals("sair")) {
+				break;
+			}
+			
+			num = Integer.parseInt(opcao);
+			
+			if (num >= -100 && num <= 100) {
+				array.add(num);
+			} else {
+				System.out.println("Valor fora do alcance");
+			}
+			
+		}
+		
+		arrayObtido = array.toArray(new Integer[array.size()]);
+		
+		return ArrayUtils.toPrimitive(arrayObtido);
 	}
 }
