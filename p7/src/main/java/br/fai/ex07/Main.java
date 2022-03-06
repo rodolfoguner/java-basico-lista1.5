f58232a741954f7fb2a0cfc4e74df747a22faea2
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-03-06 18:56:58 -0300

    feat: adicionado calculo de media

--- p7/src/main/java/br/fai/ex07/Main.java
+++ p7/src/main/java/br/fai/ex07/Main.java
@@ -16,8 +16,22 @@ public class Main {
 		int[] array1 = lerValoresArray(1);
 		int[] array2 = lerValoresArray(2);
 		int arrayCombinado[];
+		float soma = 0;
+		float media = 0;
 		
+		arrayCombinado = ArrayUtils.addAll(array1, array2);
+
+		for (int num: arrayCombinado) {
+			soma += num;
+		}
 		
+		media = soma / arrayCombinado.length;
+		
+		System.out.println("Valores do array: " + ArrayUtils.toString(array1));
+		System.out.println("Valores do array: " + ArrayUtils.toString(array2));
+		System.out.println("Arrays combinados: " + ArrayUtils.toString(arrayCombinado));
+		System.out.printf("Soma: %.0f", soma);
+		System.out.printf("\nMedia: %.2f", media);	
 	}
 	
 	private int[] lerValoresArray(int numArray) {
@@ -32,7 +46,7 @@ public class Main {
 			System.out.print("Digite um número de -100 até 100 para lista " + numArray + " ou sair: ");
 			opcao = scanner.nextLine().trim();
 
-			if (opcao.equals("sair")) {
+			if (opcao.toLowerCase().equals("sair")) {
 				break;
 			}
 			
